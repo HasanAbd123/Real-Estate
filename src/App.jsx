9e1eb2dac14d91610c55e37e539600a9cfc6600c
HasanAbd123 <en.hasan.abdulaal@gmail.com>
2025-04-22 20:43:31 +0300

    third

--- src/App.jsx
+++ src/App.jsx
@@ -6,39 +6,37 @@ import {
   RouterProvider,
   Route,
   Link,
+  createHashRouter,
 } from "react-router-dom";
 import ListPage from "./routes/listPage/ListPage";
 import Layout from "./routes/layout/Layout";
 import SinglePage from "./routes/singlePage/SinglePage";
 import Profilepage from "./routes/profilePage/Profilepage";
 function App() {
-  const router = createBrowserRouter(
-    [
-      {
-        path: "/",
-        element: <Layout />,
-        children: [
-          {
-            path: "/",
-            element: <Homepage />,
-          },
-          {
-            path: "/list",
-            element: <ListPage />,
-          },
-          {
-            path: "/:id",
-            element: <SinglePage />,
-          },
-          {
-            path: "/profile",
-            element: <Profilepage />,
-          },
-        ],
-      },
-    ],
-    { basename: "/Real-Estate/" }
-  );
+  const router = createHashRouter([
+    {
+      path: "/",
+      element: <Layout />,
+      children: [
+        {
+          path: "/",
+          element: <Homepage />,
+        },
+        {
+          path: "/list",
+          element: <ListPage />,
+        },
+        {
+          path: "/:id",
+          element: <SinglePage />,
+        },
+        {
+          path: "/profile",
+          element: <Profilepage />,
+        },
+      ],
+    },
+  ]);
   return <RouterProvider router={router} />;
 }
 
